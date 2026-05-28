0adce7e56da70089a46e2953172c8d2af4c09af3
Kha bùi <hkha0801@gmail.com>
2026-05-28 10:05:58 +0700

    feat: Login page - Register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Modal from "./components/Modal";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import logo from "./components/Logo.png";
 
 const App: React.FC = () => {
   return (
-    <>
+    <BrowserRouter>
+
       <img src={logo} className="app-logo-fixed" alt="QUEU" />
       <Navbar />
-      <Home />
-      <Modal />
       <Sidebar />
-    </>
+      <Modal />
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/*" element={
+          <>
+            <img src={logo} className="app-logo-fixed" alt="QUEU" />
+            <Navbar />
+            <Sidebar />
+            <Modal />
+          </>
+        } />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- src/components/data.tsx
+++ src/components/data.tsx
@@ -1,16 +1,4 @@
 import React from "react";
-import {
-  FaBehance,
-  FaFacebook,
-  FaLinkedin,
-  FaTwitter,
-  FaSketch,
-  FaHome,
-  FaUserFriends,
-  FaFolderOpen,
-  FaCalendarAlt,
-  FaWpforms,
-} from "react-icons/fa";
 
 
 export const links: {
